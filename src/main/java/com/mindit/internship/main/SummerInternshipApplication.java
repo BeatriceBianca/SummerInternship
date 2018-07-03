34baf6fd8fbd7539a84bbd7e4e3749438bcaf503
Beatrice Bianca Luca <bianca.luca@mindit.io>
2018-07-03 13:12:11 +0300

    Created structure

---
+++ src/main/java/com/mindit/internship/main/SummerInternshipApplication.java
@@ -0,0 +1,23 @@
+package com.mindit.internship.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@ComponentScan({""})
+@EnableJpaRepositories("com.mindit.internship.repository")
+@EntityScan(basePackages = {"com.mindit.internship.model"})
+@EnableScheduling
+@Configuration
+public class SummerInternshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SummerInternshipApplication.class, args);
+    }
+
+}
